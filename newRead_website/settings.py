4709c6210a0d6c2815025600f5af5f10b2340397
ryanlehner <ryan.lehner@gmail.com>
2023-12-29 01:13:14 -0500

    fleshing out the website a little

--- newRead_website/settings.py
+++ newRead_website/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.join(BASE_DIR, "newRead_website")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'newRead_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.join(PROJECT_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- newRead_website/views.py
+++ newRead_website/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Homepage!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+def about(request):
+    return render(request, 'about.html')
+
+def about(request):
+    return render(request, 'contact.html')
